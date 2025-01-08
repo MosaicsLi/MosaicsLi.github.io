710ba9f3bfd1155dec1db7feca69322a60b768e6
DESKTOP-HL58G6I\tamak <tamaken571@gmail.com>
2025-01-08 15:26:23 +0800

    make card to vue and fix some Css mistake

---
+++ JustSelfIntriduce/Cards.js
@@ -0,0 +1,49 @@
+const CardsVueApp = createApp({
+    data() {
+        return {
+            Cards: [],
+        };
+    },
+    mounted() {
+        this.getCardDataApi();
+    },
+    methods: {
+        async getCardDataApi() {
+            try {
+                const response = await axios.get('./JustSelfIntriduce/Jsons/Cards.json');
+                this.Cards = response.data;
+                console.log("Cards data load")
+            } catch (error) {
+                console.error('Error loading JSON data:', error);
+            }
+        }
+    },
+});
+CardsVueApp.component('cardlist', {
+    props: {
+        cardlistarray:Array,
+    },
+    template: `
+    <div class="cards">
+        <card :card="cardobject" v-for="cardobject in cardlistarray"></card>
+    </div>
+    `,
+});
+CardsVueApp.component('card', {
+    props: {
+        card: Object,  // 接收包含不同 action 資訊的陣列
+    },
+    template: `
+    <div class="card">
+        <a class="nav-link" :href="card.CardHref">
+            <div class="card-content text-white">
+                <h2 class="mt-5">{{card.Title}}</h2>
+                <p>{{card.Introduction}}</p>
+                <p v-if=card.Link.length>I steal from those web site :</P>
+                <a :href="Link" v-for="Link in card.Link">{{Link}}</a>
+            </div>
+        </a>
+    </div>
+    `,
+});
+CardsVueApp.mount('#cardsbutvue');
